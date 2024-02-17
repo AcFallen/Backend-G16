7c9763a0c0aa1f11e91fb62052deb913887a7754
Roberto <r.apaza.cornejo@gmail.com>
2024-02-17 09:50:25 -0500

    creacion de primer modelo

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Plato(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    nombre = models.TextField(null=False)
+
+    class Meta:
+        db_table = 'platos'
